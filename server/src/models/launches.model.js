9bc1e5fdb72fa6c977f4c3547acda88ea3cf9fb6
Tarun Mittal <tm6622@g.rit.edu>
2023-05-01 01:59:14 -0400

    AXIOS and querying spacex data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+
+
 const launchesDatabase = require('./launches.mongo');
 const planets = require('./planets.mongo');
 
@@ -5,10 +8,10 @@ const DEFAULT_FLIGHT_NUMBER = 100;
 
 
 const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X', 
-    rocket: 'Expire IS1', 
-    launchDate: new Date('December 27, 2030'),
+    flightNumber: 100, // flight_number
+    mission: 'Kepler Exploration X', // name
+    rocket: 'Expire IS1',  // rocket.name
+    launchDate: new Date('December 27, 2030'), // date_local
     target: 'Kepler-442 b',
     customers: ['ZTM','NASA'],
     upcoming: true,
@@ -16,6 +19,25 @@ const launch = {
 };
 
 saveLaunch(launch);
+
+const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
+
+async function loadLaunchesData() {
+    console.log('Downloading launch data...');
+    await axios.post(SPACEX_API_URL, {
+            query: {}, 
+            options: {
+                populate: [
+                    {
+                        path: rocket,
+                        select: {
+                            name: 1
+                        }   
+                    }
+                ]
+            }
+    });
+}
 
 
 async function existsLaunchWithId(launchId) {
@@ -83,8 +105,10 @@ async function abortLaunchById(launchId) {
 }
 
 module.exports = {
+    loadLaunchesData,
     existsLaunchWithId,
     getAllLaunches,
     scheduleNewLaunch,
     abortLaunchById,
+  
 };
--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8001;
 
@@ -22,6 +23,7 @@ mongoose.connection.on('error', (err) => {
 async function startServer() {
     await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
+    await loadLaunchData();
 
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`);
