43f7f6fda2834c58748e82f227f4f92435abbc3e
Tarun Mittal <tm6622@g.rit.edu>
2023-05-01 03:21:57 -0400

    pagination

--- server/src/services/query.js
+++ server/src/services/query.js
@@ -1,7 +1,8 @@
+const DEFAULT_PAGE_NUMBER = 1;
 const DEFAULT_PAGE_LIMIT = 0;
 
 function getPagination(query) {
-    const page = Math.abs(query.page) || 1;
+    const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER;
     const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
     const skip = (page - 1) * limit;
 
@@ -9,4 +10,8 @@ function getPagination(query) {
         skip,
         limit,
     };
+}
+
+module.exports = {
+    getPagination,
 }
