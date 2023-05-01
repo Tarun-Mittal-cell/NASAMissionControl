398de263f5923101e3a23a1263a300fe44705e67
Tarun Mittal <tm6622@g.rit.edu>
2023-05-01 03:19:49 -0400

    fixed pagination function

--- server/src/services/query.js
+++ server/src/services/query.js
@@ -1,6 +1,8 @@
+const DEFAULT_PAGE_LIMIT = 0;
+
 function getPagination(query) {
-    const page = Math.abs(query.page);
-    const limit = Math.abs(query.limit);
+    const page = Math.abs(query.page) || 1;
+    const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
     const skip = (page - 1) * limit;
 
     return {
